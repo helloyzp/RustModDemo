58e2172069e6cb4f7731a99b9d37e6dd2215676c
ant <1048023269@qq.com>
2021-07-12 14:37:51 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+mod lib1;
+mod lib2;
+mod lib3;
+mod lib4;
+mod lib5;
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+
+///////////////////////////////////////////////////////
+
+
+
+mod front_of_house;//使用mod关键字声明front_of_house模块，具体的定义在front_of_house.rs文件中
+
+pub fn eat_at_restaurant() {
+    //绝对路径，绝对路径必须以crate开头，因为它代码整个Module树的根节点。路径之间使用的是双冒号来表示引用
+    crate::front_of_house::hosting::add_to_waitlist();
+    //相对路径
+    front_of_house::hosting::add_to_waitlist();
+
+}
